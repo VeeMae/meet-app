1476e64e8e7980fe121349865379a3d463e7ebfa
venus_mae <veemaee@gmail.com>
2021-01-11 23:20:53 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
         this.mounted = true;
         getEvents().then((events) => {
             if (this.mounted) {
-                this.setState({ events: events, locations: extractLocations(events) });
+                this.setState({ numOfEvents: 32, events: events, locations: extractLocations(events) });
             }
         });
     }
