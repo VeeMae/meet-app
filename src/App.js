392ad0658ad8bbd15bbd6f30f7cbf327aa3eae10
Matt Dillemuth <mattdillemuth@gmail.com>
2021-01-21 17:51:24 -0500

    Fix bug causing app crash on changing input after selecting suggestion

--- src/App.js
+++ src/App.js
@@ -18,17 +18,17 @@ class App extends Component {
 
     componentDidMount() {
         this.mounted = true;
-        getEvents().then((events) => {
-            if (this.mounted) {
-                this.setState({
-                    events: events.slice(0, this.state.numOfEvents),
-                    locations: extractLocations(events)
-                });
-            }
+        getEvents().then((response) => {
+          if (this.mounted) {
+            this.setState({
+              events: response.events.slice(0, this.state.numOfEvents),
+              locations: response.locations,
+            });
+          }
         });
         window.addEventListener('offline', this.networkChangeHandler());
         window.addEventListener('online', this.networkChangeHandler());
-    }
+      }
 
     componentWillUnmount(){
         this.mounted = false;
@@ -36,29 +36,35 @@ class App extends Component {
 
     updateEvents = (location, eventCount) => {
         if (location) {
-          getEvents().then((events) => {
-            const locationEvents = (location === 'all') ?
-                events :
-                events.filter((event) => event.location === location);
-                const filteredEvents = locationEvents.slice(0, this.state.numOfEvents);
-                return this.setState({
-                 events: filteredEvents,
-                 currentLocation: location,
-                 locations: events.locations
-                });
+          getEvents().then((response) => {
+            const locationEvents =
+              location === 'all'
+                ? response.events
+                : response.events.filter((event) => event.location === location);
+            const filteredEvents = locationEvents.slice(0, this.state.numOfEvents);
+            return this.setState({
+              events: filteredEvents,
+              currentLocation: location,
+              locations: response.locations,
             });
+          });
         } else {
-            getEvents().then((events) => {
-                const locationEvents = (this.state.currentLocation === 'all') ? events : events.filter((event) => event.location === this.state.currentLocation);
-                const filteredEvents = locationEvents.slice(0, eventCount);
-                return this.setState({
-                    events: filteredEvents,
-                    numOfEvents: eventCount,
-                    locations: events.locations
-                });
+          getEvents().then((response) => {
+            const locationEvents =
+              this.state.currentLocation === 'all'
+                ? response.events
+                : response.events.filter(
+                    (event) => event.location === this.state.currentLocation
+                  );
+            const filteredEvents = locationEvents.slice(0, this.state.numOfEvents);
+            return this.setState({
+              events: filteredEvents,
+              numOfEvents: eventCount,
+              locations: response.locations,
             });
+          });
         }
-    }
+      };
 
     networkChangeHandler = () => {
         if (navigator.onLine === false) {
