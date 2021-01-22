f1896bde1cb47092f19d28073a41e198c8f722b9
venus_mae <veemaee@gmail.com>
2021-01-22 14:55:07 -0800

    Update pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -21,13 +21,12 @@ const EventGenre = ({ events }) => {
 
     const colors = ['#6D6875', '#B5838D', '#FFB4A2', '#2A2E45', '#81A094']
 
-
     return (
         <ResponsiveContainer height={400}>
             <PieChart>
-                <Pie data={data} cx="50%" cy="50%" labelLine={false} legendType="square" outerRadius={80} fill="#8884d8" datakey="value" label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}>
+                <Pie data={data} cx="45%" cy="50%" labelLine={false} legendType="square" outerRadius={80} fill="#8884d8" datakey="value" label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}>
                     {
-                        data.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index]}/>)
+                        data.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index % colors.length]} name={entry.name}/>)
                     }
                 </Pie>
                 <Legend layout="horizontal" align="center" verticalAlign="top">
