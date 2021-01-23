8baa5e029d445bf0a65465fa99a704b5d0d98e24
venus_mae <veemaee@gmail.com>
2021-01-22 19:19:05 -0800

    Update pie chart and App styles

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -13,7 +13,7 @@ const EventGenre = ({ events }) => {
     const getData = () => {
         const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
         const data = genres.map((genre) => {
-            const value = events.filter(({ summary }) => summary.split(' ' || '!').includes(genre)).length;
+            const value = events.filter(({ summary }) => summary.split(' ').includes(genre)).length;
             return { name: genre, value };
         })
     return data;
@@ -24,7 +24,7 @@ const EventGenre = ({ events }) => {
     return (
         <ResponsiveContainer height={400}>
             <PieChart>
-                <Pie data={data} cx="45%" cy="50%" labelLine={false} legendType="square" outerRadius={80} fill="#8884d8" datakey="value" label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}>
+                <Pie data={data} cx="47%" cy="50%" labelLine={false} legendType="square" outerRadius={80} fill="#8884d8" datakey="value" label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}>
                     {
                         data.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index % colors.length]} name={entry.name}/>)
                     }
